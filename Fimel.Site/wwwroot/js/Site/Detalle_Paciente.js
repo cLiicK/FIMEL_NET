3f21529093e84c0b5d7c68f44f43cd911db53a70
Ignacio Javier Quintana Asenjo <ignacioquintanaa@gmail.com>
2025-10-17 08:57:00 -0300

    correccion

---
+++ Fimel.Site/wwwroot/js/Site/Detalle_Paciente.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    var rutPaciente = $('#hiddenRutPaciente').val();
+    var numDocumento = $('#hiddenNumDocumento').val();
+    var tipoDocumento = $('#hiddenTipoDocumento').val();
+
+    if (rutPaciente && tipoDocumento === "RUT") {
+        ObtenerConsultasAnteriores(rutPaciente, "RUT");
+    } else if (numDocumento && tipoDocumento !== "RUT") {
+        ObtenerConsultasAnteriores(numDocumento, tipoDocumento);
+    }
+});
